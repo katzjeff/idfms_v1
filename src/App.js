f0cbfdadc3e8eafcfe6d8e7ccf11d343770ddff7
Jeffrey <aligula.jeff@gmail.com>
2022-12-31 12:44:07 +0300

    added contact form

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 // import RegisterUser from "./pages/RegisterUser";
-import UnderConstruction from "./pages/UnderConstruction";
+// import UnderConstruction from "./pages/UnderConstruction";
 import Terms from "./pages/Terms";
 
 import Error from "./pages/Error";
 import { Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -15,7 +16,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signin" element={<SignIn />} />
-        <Route path="/contact" element={<UnderConstruction />} />
+        <Route path="/Contact" element={<Contact />} />
         <Route path="/terms" element={<Terms />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="*" element={<Error />} />
